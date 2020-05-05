29f52947e2b66861808b4449a7870073058051af
sbooeshaghi <sbooeshaghi@gmail.com>
2020-05-05 10:14:43 -0700

    bustools correct now corrects each half of the barcode to hamming distance 1. This currently only works for barcodes with lengths that are a multiple of 4. This will be fixed in the future.

--- src/bustools_correct.cpp
+++ src/bustools_correct.cpp
@@ -37,7 +37,15 @@ int search_for_mismatch(const Roaring& r, const size_t bc, const uint64_t b, uin
   return counts;
 }
 
+bool check_distance(std::vector<std::pair<Roaring, Roaring>> & correct, uint64_t b, const size_t bc_len){
+    // TODO: create a function that takes in a correct matrix and a partial barcode
+    // and tells you true or false if it is within one hamming distance of the half-whitelist
+    // Question: can I correct the first half? Can I correct the second half? Is each half
+    // in the "half"-whitelist? Is the concatenated "full" barcode in the whitelist?
+  return false;
+}
 
+  
 void bustools_correct(Bustools_opt &opt) {
   uint32_t bclen = 0; 
   uint32_t wc_bclen = 0;
@@ -56,7 +64,10 @@ void bustools_correct(Bustools_opt &opt) {
 
   bool dump_bool = opt.dump_bool;
 
-  std::ofstream of(opt.dump);
+  std::ofstream of;
+  if (dump_bool){
+    of.open(opt.dump);
+  }
 
   std::ifstream wf(opt.whitelist, std::ios::in);
   std::string line;
@@ -77,18 +88,55 @@ void bustools_correct(Bustools_opt &opt) {
 
   // split barcode into upper and lower half
   size_t bc2 = (wc_bclen+1)/2; 
+  size_t bc4 = (bc2/2);
 
   std::vector<std::pair<Roaring,Roaring>> correct(1ULL<<(2*bc2)); // 4^(bc/2) possible barcodes
+
+  std::vector<std::pair<Roaring,Roaring>> lower_correct(1ULL<<(2*bc4)); // 4^(bc/4) possible barcodes
+  std::vector<std::pair<Roaring,Roaring>> upper_correct(1ULL<<(2*bc4)); // 4^(bc/4) possible barcodes
+
+
 
   uint64_t mask_size = (1ULL << (2*bc2));
   uint64_t lower_mask = (1ULL<<(2*bc2))-1;
   uint64_t upper_mask = (1ULL<<(2*(wc_bclen-bc2)))-1;
+
+  uint64_t l_lower_mask = (1ULL<<(2*bc4))-1;
+  uint64_t u_lower_mask = (1ULL<<(2*bc4))-1;
+
+  uint64_t l_upper_mask = (1ULL<<(2*(bc2-bc4)))-1;
+  uint64_t u_upper_mask = (1ULL<<(2*(bc2-bc4)))-1;
+
+  // barcode is split into 4 peices of equal size, check each half in the following way:
+  // AGCC AAGA GAGG GCAT
+  // UU   LU    UL  LL
+  // U: upper, L: lower
+  // create all hamming 1 distance variants of the LL half and check to see if
+  // any one of those variants (concatenated with the UL) is in the "half"-whitelist
+  // Do the same for the UU and LU half.
+
+
+
   for (uint64_t b : wbc) {
     uint64_t lb = b & lower_mask;
     uint64_t ub = (b>>(2*bc2)) & upper_mask;
 
+    uint64_t llb = b & l_lower_mask;
+    uint64_t ulb = (b>>(2*bc4)) & u_lower_mask;
+
+    uint64_t lub = (b>>(2*(2*bc4))) & l_upper_mask;
+    uint64_t uub = (b>>(3*(2*bc4))) & u_upper_mask;
+
+    // std::cout << "ub: " << binaryToString(ub, wc_bclen/2) << "\t" << "lb: " << binaryToString(lb, wc_bclen/2) << "\n";
+
     correct[ub].second.add(lb);
     correct[lb].first.add(ub);
+
+    lower_correct[ulb].second.add(llb);
+    lower_correct[llb].first.add(ulb);
+
+    upper_correct[uub].second.add(lub);
+    upper_correct[lub].first.add(uub);
   }
 
   std::streambuf *buf = nullptr;
@@ -123,6 +171,9 @@ void bustools_correct(Bustools_opt &opt) {
       outheader_written = true;
     }
 
+
+
+
     if (bclen == 0) {
       bclen = h.bclen;
 
@@ -144,23 +195,69 @@ void bustools_correct(Bustools_opt &opt) {
       if (rc == 0) {
         break;
       }
-      nr +=rc;
+      nr += rc;
 
       for (size_t i = 0; i < rc; i++) {
         bd = p[i];
         auto it = wbc.find(bd.barcode);
         if (it != wbc.end()) { 
           stat_white++;
-          //bd.count = 1;
           bus_out.write((char*) &bd, sizeof(bd));
-        } else {  
+        } else {  // TODO: there is code redundancy in this else statement, fix.
           uint64_t b = bd.barcode;
-          uint64_t lb = b & lower_mask;
+
+          uint64_t lb =           b & lower_mask;
           uint64_t ub = (b>>(2*bc2)) & upper_mask;   
-          uint64_t lbc=0,ubc=0;
-          int correct_lower = search_for_mismatch(correct[ub].second,bc2,lb,lbc);
-          int correct_upper = search_for_mismatch(correct[lb].first,wc_bclen - bc2,ub,ubc);
+
+          uint64_t upper_corrected = ub;
+          uint64_t lower_corrected = lb;
+
+          uint64_t llb =            b & l_lower_mask;
+          uint64_t ulb = (b>>(2*bc4)) & u_lower_mask;
+
+          uint64_t lub = (b>>(2*(2*bc4))) & l_upper_mask;
+          uint64_t uub = (b>>(3*(2*bc4))) & u_upper_mask;
+
+          uint64_t lbc=0,  ubc=0;
+          uint64_t llbc=0, ulbc=0;
+          uint64_t lubc=0, uubc=0;
+
+          // std::cout << binaryToString(uub, bc4) << "\t" << binaryToString(lub, bc4) << "\t" 
+          //           << binaryToString(ulb, bc4) << "\t" << binaryToString(llb, bc4) << "\n";
+
+          int correct_l_lower = search_for_mismatch(lower_correct[ulb].second,bc4, llb, llbc);
+          int correct_u_lower = search_for_mismatch(lower_correct[llb].first, bc4, ulb, ulbc);
+
+          int nc_lower = correct_l_lower + correct_u_lower;
+
+          if (nc_lower == 1){
+            if (correct_l_lower == 1){
+              lower_corrected = (ulb<<(2*bc4)) | llbc ;
+            } else if (correct_u_lower == 1){
+              lower_corrected = (ulbc<<(2*bc4)) | llb;
+            }
+          }
+          
+          int correct_l_upper = search_for_mismatch(upper_correct[uub].second, bc4, lub, lubc);
+          int correct_u_upper = search_for_mismatch(upper_correct[lub].first,  bc4, uub, uubc);
+
+          int nc_upper = correct_l_upper + correct_u_upper;
+
+          if (nc_upper == 1){
+            if (correct_l_upper == 1){
+              upper_corrected = (uub<<(2*bc4)) | lubc;
+            } else if (correct_u_upper == 1){
+              upper_corrected = (uubc<<(2*bc4)) | lub;
+            }
+          }
+
+
+          int correct_lower = search_for_mismatch(correct[upper_corrected].second,bc2,lb,lbc);
+          int correct_upper = search_for_mismatch(correct[lower_corrected].first,wc_bclen - bc2,ub,ubc);
+
           int nc = correct_lower + correct_upper;
+          // AGCCAAGA GAGGGCAT
+
 
           if (nc == 0 || nc > 2) {
             stat_uncorr++;
@@ -188,10 +285,10 @@ void bustools_correct(Bustools_opt &opt) {
               }
               bd.barcode = b_corrected;
               stat_corr++;
+              bus_out.write((char*) &bd, sizeof(bd));   
             } 
-            // bd.count = 1;
-            bus_out.write((char*) &bd, sizeof(bd));   
           } else if (nc==2) {
+
             if (correct_upper == 1 && correct_lower == 1) {
               uint64_t b_corrected = (ubc << (2*bc2)) | lbc; 
 
@@ -203,8 +300,9 @@ void bustools_correct(Bustools_opt &opt) {
               }
               bd.barcode = b_corrected;
               stat_corr_2++;
+              bus_out.write((char*) &bd, sizeof(bd));
             }
-            bus_out.write((char*) &bd, sizeof(bd));
+            
           }
         }
       }
@@ -221,7 +319,9 @@ void bustools_correct(Bustools_opt &opt) {
   if (!opt.stream_out) {
     busf_out.close();
   }
-  of.close();
+  if (opt.dump_bool){
+    of.close(); // if of is open
+  }
 
   delete[] p; p = nullptr;
 }
