64e4026bb0e839410e3e0e4c35986a19123c6246
Johan Gustafsson <gustajo@chalmers.se>
2020-05-14 15:53:42 +0200

    feat: Added empty predict function, will be filled in later.

---
+++ src/bustools_predict.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+
+#include "Common.hpp"
+#include "BUSData.h"
+
+#include "bustools_predict.h"
+
+void bustools_predict(Bustools_opt &opt) {
+	
+}
