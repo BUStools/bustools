df40fa87010d4c5e03c208772e52a1b66a81af98
Delaney Sullivan <delaneyk.sullivan@gmail.com>
2023-10-25 15:14:08 -0700

    label barcode prefixes for capture

--- src/BUSData.cpp
+++ src/BUSData.cpp
@@ -5,6 +5,7 @@
 #include <unordered_map>
 #include <sstream>
 #include <iostream>
+#include <limits>
 
 uint64_t stringToBinary(const std::string &s, uint32_t &flag) {
   return stringToBinary(s.c_str(), s.size(), flag);
@@ -312,7 +313,16 @@ bool parseBcUmiCaptureList(const std::string &filename, std::unordered_set<uint6
   std::string inp;
   uint32_t flag; // Unused
   while (getline(inf, inp)) {
-    captures.insert(stringToBinary(inp, flag));
+    uint64_t binary_val;
+    if (inp.back() == '*' && inp.length() == 17) { // 16-bp barcode that ends in *
+	    inf.pop_back(); // Remove * from end of string
+	    binary_val = stringToBinary(inp, flag);
+	    binary_val |= (static_cast<uint64_t>(1) << 63); // Set MSB to 1 if * found at end of string (to label the barcode as "prefix")
+	    captures.insert(std::numeric_limits<uint64_t>::max()); // Tells us to look for prefix barcodes
+    } else {
+	    binary_val = stringToBinary(inp, flag);
+    }
+    captures.insert(binary_val);
   }
 
   return true;
@@ -348,7 +358,6 @@ bool parseFlagsCaptureList(const std::string &filename, std::unordered_set<uint6
 
 bool parseGenes(const std::string &filename, const u_map_<std::string, int32_t> &txnames, std::vector<int32_t> &genemap, u_map_<std::string, int32_t> &genenames) {
   std::ifstream inf(filename.c_str());
-  bool ret = true;
 
   std::string line, t;
   line.reserve(10000);
@@ -370,12 +379,10 @@ bool parseGenes(const std::string &filename, const u_map_<std::string, int32_t> 
         gi = git->second;
       }
       genemap[i] = gi;
-    } else {
-      ret = false;
     }
   }
 
-  return ret;
+  return true;
 }
 
 bool parseGenesList(const std::string& filename, std::vector<std::string>& geneNames) {
